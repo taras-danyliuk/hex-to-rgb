e5a3b7f14df8ee077ca0956a0e6e60518e151ca8
Taras Danyliuk <t.danyliuk@relevant.software>
2022-03-09 14:56:22 +0200

    finish project

--- main.js
+++ main.js
@@ -1,21 +1,80 @@
-(function() {
-  const hexInput = document.querySelector("#hex");
-  const rgbInput = document.querySelector("#rgb");
+(function () {
+  const hexRegExShort = /[\da-f]{3}/i;
+  const hexRegExLong = /[\da-f]{6}/i;
+  const rgbReplace = /[rgba()]/g;
+  const rgbReplaceComma = /,\s?/g;
 
 
-  // Listen to changes in HEX input
-  hexInput.addEventListener("input", function(event) {
-    let value = event.target.value.replace(/[^\w\d]/g, "");
+  const inputs = document.querySelectorAll(".input");
+  const firstInput = document.querySelector("#first");
+  const secondInput = document.querySelector("#second");
 
-    // Remove everything except digits and words
-    value = value.replace(/[^\w\d]/g, "");
-    
-    console.log(value, "hex")
+  // Add listeners
+  inputs.forEach(input => {
+    input.addEventListener("input", event => {
+      let value = event.target.value;
+
+      // RGB value
+      if (
+        value.includes(" ") ||
+        value.includes(",")
+      ) {
+        value = value.replace(rgbReplace, "").replace(rgbReplaceComma, " ").split(" ");
+
+        if (value.length >= 3) setValues(event.target, value.slice(0, 3), "rgb");
+        else resetValues(event.target);
+      }
+      // HEX value
+      else {
+        value = value.replace(/[^\w\d]/g, "");
+
+        if (
+          (hexRegExShort.test(value) && value.length === 3) ||
+          (hexRegExLong.test(value) && value.length === 6)
+        ) {
+          if (value.length === 3) value = value.split("").reduce((r, c) => r + `${c}${c}`, "");
+          setValues(event.target, `#${value}`, "hex");
+        } else {
+          resetValues(event.target)
+        }
+      }
+    });
   });
 
+  function resetValues(target) {
+    document.body.style.backgroundColor = "#f7f7f8";
+    document.documentElement.style.setProperty("--color", "#000000");
+    if (target === firstInput) secondInput.value = "";
+    else firstInput.value = "";
+  }
 
-  // Listen to changes in RGB input
-  rgbInput.addEventListener("input", function(event) {
-    console.log(event.target.value, "hex")
-  })
-})()
+  function setValues(target, color, type) {
+    const oppositeValue = type === "hex" ? hexToRgb(color) : rgbToHex(...color);
+
+    document.body.style.backgroundColor = oppositeValue;
+    document.documentElement.style.setProperty("--color", getContrastColor(type === "hex" ? color : oppositeValue));
+
+    if (target === firstInput) secondInput.value = oppositeValue;
+    else firstInput.value = oppositeValue;
+  }
+
+  function rgbToHex(r, g, b) {
+    const hexR = r.toString(16);
+    const hexG = g.toString(16);
+    const hexB = b.toString(16);
+
+    return "#" + `0${hexR}`.slice(-2) + `0${hexG}`.slice(-2) + `0${hexB}`.slice(-2);
+  }
+
+  function hexToRgb(hex) {
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return `rgb(${parseInt(result[1], 16)},${parseInt(result[2], 16)}, ${parseInt(result[3], 16)})`;
+  }
+
+  function getContrastColor(hex) {
+    const [_, red, green, blue] = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return (parseInt(red, 16) * 0.299 + parseInt(green, 16) * 0.587 + parseInt(blue, 16) * 0.114) > 150 ?
+      "#000000" :
+      "#ffffff";
+  }
+})();
